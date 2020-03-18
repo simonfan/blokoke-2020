8c1389b707ad057b777e1848ad94d49e81da05f6
Simon <simonxd@gmail.com>
2020-03-18 01:36:31 -0300

    Updates

---
+++ precache-manifest.d5efbd0c59a4a5329938afcf4d031ea6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e745ab929c868094f8f6cbb3786f2ce",
+    "url": "/blokoke-2020/index.html"
+  },
+  {
+    "revision": "c20da63624251fd63de6",
+    "url": "/blokoke-2020/static/css/2.1ad75add.chunk.css"
+  },
+  {
+    "revision": "6bc6306f63d84765a82f",
+    "url": "/blokoke-2020/static/css/main.a580f6cf.chunk.css"
+  },
+  {
+    "revision": "c20da63624251fd63de6",
+    "url": "/blokoke-2020/static/js/2.7f538e73.chunk.js"
+  },
+  {
+    "revision": "284d116f86c8eb92a654f6318b2938d9",
+    "url": "/blokoke-2020/static/js/2.7f538e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc6306f63d84765a82f",
+    "url": "/blokoke-2020/static/js/main.baeca244.chunk.js"
+  },
+  {
+    "revision": "20df7b667a932383928e",
+    "url": "/blokoke-2020/static/js/runtime-main.f3036462.js"
+  }
+]);
