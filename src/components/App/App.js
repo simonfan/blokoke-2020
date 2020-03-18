42fd19755e29ecc42abbdfb951f7b8b3fd08972c
Simon <simonxd@gmail.com>
2020-03-18 01:34:37 -0300

    Preparing build

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,20 +6,26 @@ import { DataContext } from '../DataContext/DataContext'
 import IMAGES from '../../data/images.json'
 import mediumZoom from 'medium-zoom'
 
+const PHOTOS_ROOT_URL = process.env.REACT_APP_PHOTOS_ROOT_URL
+
 export const App = ({
   sections
 }) => {
-  const zoomRef = React.useRef(mediumZoom({ background: 'rgba(0, 0, 0, 0.9)', margin: 0 }))
+  const zoomRef = React.useRef(mediumZoom({
+    background: 'rgba(0, 0, 0, 0.9)',
+    margin: 0,
+    scrollOffset: 0,
+  }))
 
   return <DataContext.Provider value={{
       zoomRef,
       resolveImageUrls: ({ id }) => ({
-        placeholderSrc: `photos/miniature/${id}`,
-        zoomSrc: `photos/2000w/${id}`,
-        srcset: `photos/900w/${id} 900w,
-          photos/1400w/${id} 1400w,
-          photos/2000w/${id} 2000w,
-          photos/full/${id}`
+        placeholderSrc: `${PHOTOS_ROOT_URL}/miniature/${id}`,
+        zoomSrc: `${PHOTOS_ROOT_URL}/2000w/${id}`,
+        srcset: `${PHOTOS_ROOT_URL}/900w/${id} 900w,
+          ${PHOTOS_ROOT_URL}/1400w/${id} 1400w,
+          ${PHOTOS_ROOT_URL}/2000w/${id} 2000w,
+          ${PHOTOS_ROOT_URL}/full/${id}`
       }),
       getImageData: ({ id }) => IMAGES[id]
     }}>
